91935ae279ccb9ea36c9696b1a62653bac82ad21
jo-gentile <jose.gen86@gmail.com>
2025-05-26 17:58:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ zonas.forEach((zona, index) => {
 });
 
 // --- LOGIN POR EMAIL DESDE GOOGLE SHEETS ---
-const API_URL = https://script.google.com/macros/s/AKfycbxCT2fQW1h7vE02ai2BpGqfQ_PTAhCP8H4StFjL-zne5tlKovf1vdWo7EWguwjodmJkqA/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxCT2fQW1h7vE02ai2BpGqfQ_PTAhCP8H4StFjL-zne5tlKovf1vdWo7EWguwjodmJkqA/exec";
 
 function mostrarLogin() {
   const form = document.getElementById("loginForm");
