28fa9c1ce5d07defa30bd8f0d48e4b822da6768f
jo-gentile <jose.gen86@gmail.com>
2026-01-20 18:09:55 -0300

    vistaaaa

--- admin.js
+++ admin.js
@@ -54,7 +54,7 @@ function mostrarAccionesZona(numeroZona) {
         // 2. Cargamos el nuevo sistema de iframe
         contenedor.innerHTML = `
             <iframe src="acciones_zonas.html?zona=${numeroZona}" 
-                    style="width: 100%; height: 600px; border: none; background: transparent;">
+                    style="width: 100%; min-height: 1800px; border: none; background: transparent;"
             </iframe>
         `;
 
